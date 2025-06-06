4131a2383f4c38bab5c291379145341b28ad7918
akshay9035 <akshaykumar90350993@gmail.com>
2025-06-06 23:54:49 +0530

    altered sript

--- create_tables.sql
+++ create_tables.sql
@@ -1,14 +1,10 @@
-create database if not exists CompanyDB;
 
-use CompanyDB;
-
-create table if not exists Employees(
-ID int primary key auto_increment,
+create table Employee(
+ID number primary key ,
 firstname varchar(50),
 lastname varchar(50),
 department varchar(50)
 );
 
-insert into Employees(firstname,lastname,department) values
-('akshay','kumar','IT'),
-('mukesh','yadav','QA');
+insert into Employee values(1,'akshay','kumar','IT');
+insert into Employee values(2,'mukesh','yadav','QA');
