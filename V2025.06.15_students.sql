d2f70814de8b26b57992b71473984e96156fbc8b
akshay9035 <akshaykumar90350993@gmail.com>
2025-06-15 20:02:24 +0530

    added an table on 2025-06-15

---
+++ V2025.06.15_students.sql
@@ -0,0 +1,10 @@
+drop table students_tb;
+
+create table students_tb(
+students_id int,
+students_name varchar(30)
+
+);
+
+insert into students_tb values(1,'akshay kumar');
+insert into students_tb values(2,'kiran kumar');
